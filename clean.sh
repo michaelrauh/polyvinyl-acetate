606eb07000046e0bb631e59b57d90357571dc911
michaelrauh <michaelrauh@ymail.com>
2022-03-19 15:47:17 -0400

    fix clean to remove worker

--- clean.sh
+++ clean.sh
@@ -4,6 +4,7 @@ helm uninstall postgres-k
 helm uninstall rabbit-k
 kubectl delete deploy pvac-web
 kubectl delete deploy pvac-relay
+kubectl delete deploy pvac-worker
 kubectl delete pod --all
 kubectl delete pvc --all
 kubectl delete pv --all
