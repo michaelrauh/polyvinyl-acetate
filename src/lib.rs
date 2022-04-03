fa4a13ad4c8d3b5aa7775f7444781272194b608e
michaelrauh <michaelrauh@ymail.com>
2022-04-03 18:49:21 -0400

    promote ortho to be first class citizen. Add missing tests. Stop using name shortening for orthotope as that is a distinct concept in this design

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use schema::{sentences, todos};
 mod book_todo_handler;
+mod ortho;
 mod pair_todo_handler;
 mod sentence_todo_handler;
 pub mod web_helper;
--- src/pair_todo_handler.rs
+++ src/pair_todo_handler.rs
@@ -1,8 +1,9 @@
 use std::{
-    collections::{hash_map::DefaultHasher, BTreeMap, HashSet},
+    collections::{hash_map::DefaultHasher, BTreeMap},
     hash::{Hash, Hasher},
 };
 
+use crate::ortho::Location;
 use crate::{
     create_todo_entry,
     diesel::query_dsl::filter_dsl::FilterDsl,
@@ -39,73 +40,9 @@ pub fn handle_pair_todo(todo: Todo) -> Result<(), anyhow::Error> {
     })
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Ortho {
-    info: BTreeMap<Location, String>,
-}
-
-impl Ortho {
-    fn get_origin(&self) -> String {
-        let (_k, v) = self
-            .info
-            .iter()
-            .find(|(k, _v)| k.length() == 0)
-            .expect("all orthos should have an origin");
-        v.to_string()
-    }
-
-    fn get_hop(&self) -> HashSet<String> {
-        self.info
-            .iter()
-            .filter_map(|(k, v)| {
-                if k.length() == 1 {
-                    Some(v.to_string())
-                } else {
-                    None
-                }
-            })
-            .collect()
-    }
-
-    fn get_contents(&self) -> HashSet<String> {
-        self.info.iter().map(|(_k, v)| v.to_string()).collect()
-    }
-
-    fn new(a: String, b: String, c: String, d: String) -> Ortho {
-        let inner_loc_a = BTreeMap::default();
-        let mut inner_loc_b = BTreeMap::default();
-        let mut inner_loc_c = BTreeMap::default();
-        let mut inner_loc_d = BTreeMap::default();
-        let mut info = BTreeMap::default();
-
-        inner_loc_b.insert(b.clone(), 1);
-        inner_loc_c.insert(c.clone(), 1);
-        inner_loc_d.insert(b.clone(), 1);
-        inner_loc_d.insert(c.clone(), 1);
-
-        let loc_a = Location { info: inner_loc_a };
-        let loc_b = Location { info: inner_loc_b };
-        let loc_c = Location { info: inner_loc_c };
-        let loc_d = Location { info: inner_loc_d };
-
-        info.insert(loc_a, a);
-        info.insert(loc_b, b);
-        info.insert(loc_c, c);
-        info.insert(loc_d, d);
-
-        Ortho { info }
-    }
-}
-
-#[derive(Serialize, Deserialize, Ord, PartialOrd, PartialEq, Eq)]
-pub struct Location {
-    info: BTreeMap<String, usize>,
-}
-
-impl Location {
-    fn length(&self) -> usize {
-        self.info.iter().fold(0, |acc, (_cur_k, cur_v)| acc + cur_v)
-    }
+    pub(crate) info: BTreeMap<Location, String>,
 }
 
 fn new_orthos(pair: Pair) -> Vec<NewOrthotope> {
