1598b41108565694e1a4510b41e69c4cd90b0f9d
michaelrauh <michaelrauh@ymail.com>
2022-07-30 13:11:55 -0400

    small updates to scripts

--- provision_prod.sh
+++ provision_prod.sh
@@ -1,2 +1,2 @@
-doctl kubernetes cluster create pvac-cluster --size s-4vcpu-8gb-intel --count 2
+doctl kubernetes cluster create pvac-cluster --size s-4vcpu-8gb-intel --count 5
 doctl kubernetes cluster registry add pvac-cluster
---
+++ scale_worker.sh
@@ -0,0 +1,1 @@
+kubectl scale deployment/pvac-worker --replicas=10
