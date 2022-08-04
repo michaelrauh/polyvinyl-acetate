144fe27af5aa4a8028a18d234760906478acac25
michaelrauh <michaelrauh@ymail.com>
2022-08-04 14:29:12 -0400

    use smaller cloud resources, but use more of them. Increase worker replication

--- provision_prod.sh
+++ provision_prod.sh
@@ -1,2 +1,2 @@
-doctl kubernetes cluster create pvac-cluster --size s-4vcpu-8gb-intel --count 5
+doctl kubernetes cluster create pvac-cluster --size s-1vcpu-2gb-intel --count 10
 doctl kubernetes cluster registry add pvac-cluster
