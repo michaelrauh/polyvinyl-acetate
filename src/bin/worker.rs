3a28199d47cc1d7cd9de90bf6b1bc92cf17da3b9
michaelrauh <michaelrauh@ymail.com>
2022-09-13 18:54:42 -0400

    get jaeger to work on host machine

--- src/bin/worker.rs
+++ src/bin/worker.rs
@@ -4,8 +4,12 @@ use amiquip::{
 };
 use polyvinyl_acetate::models::Todo;
 use polyvinyl_acetate::worker_helper;
-use std::{env, io};
-use tracing::{span_enabled, Level, trace_span};
+use std::{env};
+
+use opentelemetry::global;
+use tracing_subscriber::{
+    fmt, layer::SubscriberExt, util::SubscriberInitExt,
+};
 
 fn main() {
     get().expect("Rabbit should not err");
@@ -33,17 +37,31 @@ fn get() -> Result<(), anyhow::Error> {
     channel.qos(0, 1, false)?;
 
     let consumer = queue.consume(ConsumerOptions::default())?;
-    let subscriber = tracing_subscriber::FmtSubscriber::builder()
-        .with_max_level(Level::TRACE)
-        .finish();
-    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
-    let number_of_teams: i32 = 3;
-    tracing::info!(number_of_teams, "We've got {} teams!", number_of_teams);
+    
+    
+    // Allows you to pass along context (i.e., trace IDs) across services
+global::set_text_map_propagator(opentelemetry_jaeger::Propagator::new());
+// Sets up the machinery needed to export data to Jaeger
+// There are other OTel crates that provide pipelines for the vendors
+// mentioned earlier.
+let tracer = opentelemetry_jaeger::new_pipeline()
+.with_agent_endpoint("host.docker.internal:6831")
+    .with_service_name("pvac")
+    .install_simple().expect("tracer made");
+
+// Create a tracing layer with the configured tracer
+let opentelemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
+// The SubscriberExt and SubscriberInitExt traits are needed to extend the
+// Registry to accept `opentelemetry (the OpenTelemetryLayer type).
+tracing_subscriber::registry()
+    .with(opentelemetry)
+    // Continue logging to stdout
+    .with(fmt::Layer::default())
+    .try_init().expect("subscribed");
 
     for (i, message) in consumer.receiver().iter().enumerate() {
         println!("number of messages: {}", i);
-        // tracing::info!(number_of_teams, "We've got {} teams!", number_of_teams);
-        let _ = tracing::info_span!("here");
         match message {
             ConsumerMessage::Delivery(delivery) => {
                 let todo: Todo = bincode::deserialize(&delivery.body)?;
